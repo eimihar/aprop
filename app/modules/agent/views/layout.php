bab5a63e2d3dea660f7875245a9f875a2b04b734
eimihar <newrehmi@gmail.com>
2017-01-11 03:24:32 +0800

    responsive admin

--- app/modules/agent/views/layout.php
+++ app/modules/agent/views/layout.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script type="text/javascript" src="/js/jquery.min.js"></script>
 <!--        <script type="text/javascript" src="/js/bootstrap.min.js"></script>-->
         <link rel="stylesheet" href="/css/bootstrap.min.css" type="text/css" />
