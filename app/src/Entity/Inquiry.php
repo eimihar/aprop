67766bc3ac5dc1ceb72e3aa4c040247250cb6c28
eimihar <newrehmi@gmail.com>
2017-01-10 23:00:11 +0800

    remoe

--- app/src/Entity/Inquiry.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Entity;
-
-class Inquiry extends BaseEntity
-{
-    protected $table = 'inquiry';
-}
--- app/src/Entity/InquiryProject.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace App\Entity;
-
-class InquiryProject extends BaseEntity
-{
-    protected $table = 'inquiry_project';
-}
